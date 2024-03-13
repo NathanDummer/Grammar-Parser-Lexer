1cb04b0d33056f61012d4056c884e092caa88f0e
NathanDummer <139890511+NathanDummer@users.noreply.github.com>
2024-03-12 21:24:32 -0400

    Passes "testSimpleStatements" testcases

--- project1_parser.py
+++ project1_parser.py
@@ -10,18 +10,28 @@ class Lexer:
     #helper function that splits parenthesis from variable or values
     def splitLst(self):
         lst = self.lst
-        for i in range(len(lst) -1):
-            last = len(lst[i])-1
+
+        i = 0
+        length = len(lst)
+        while i < length:
+            last = len(lst[i]) - 1
+
             if lst[i][0] == '(':
                 lst[i] = lst[i][1:]
                 lst = lst[:i] + ['('] + lst[i:]
+                length += 1
         
-            elif lst[i][last] == ')':
+            elif lst[i][last] == ')' and len(lst[i]) > 1:
                 lst[i] = lst[i][:last]
                 lst = lst[:i+1] + [')'] + lst[i+1:]
+                length += 1
 
+            i += 1
+        
+        
         self.lst = [i for i in lst if i != '']
         self.length = len(self.lst)
+        
 
     # move the lexer position and identify next possible tokens.
     def get_token(self):
@@ -65,6 +75,7 @@ class Parser:
 
     # function to parse the entire program
     def parse(self):
+        self.lexer.splitLst()
         retStr = ""
         while self.lexer.position < self.lexer.length - 1:
             str = self.lexer.get_token()
@@ -94,54 +105,127 @@ class Parser:
         iter = self.lexer.position + 1
         
         # checks until end of the statement
-        while iter < self.lexer.length - 1:
+        while iter < self.lexer.length:
             curr = self.lexer.lst[iter]
+            #this checks for parenthesis and ignores them if so
+            
+            while self.lexer.lst[iter - 1] == '(' or self.lexer.lst[iter] == ')':
+                iter += 1
+                if iter < self.lexer.length - 1:
+                    curr = self.lexer.lst[iter]
+                else: break
+            
             if curr not in arithm:
                 break
             iter += 2
-
+        
         lst = self.lexer.lst[self.lexer.position + 2:iter]
-        print("full: ", self.lexer.lst[self.lexer.position:iter])
-        print("input: ", lst)
+        #print("full: ", self.lexer.lst[self.lexer.position:iter])
+        #print("input: ", lst)
 
         retStr = "('=', '" + self.lexer.lst[self.lexer.position] + "', " + self.arithmetic_expression(lst) + ')'
-        print("return: ", retStr)
+        #print("return: ", retStr, "\n")
         self.lexer.position = iter
+        return retStr
         
-    
     # parse arithmetic expressions
     def arithmetic_expression(self, lst):
-        
         if len(lst) == 1:
             if lst[0].isdigit(): return lst[0]
             else: return "'" + lst[0] + "'"
 
         str = ""
-        for i in range(len(lst)-2):
-            
+        while len(lst) > 1:
             # using recursion in case of parenthesis within arithmetic expressions
-            if lst[i] == '(':
-                str = "'" + lst[i+1] + "', " + self.arithmetic_expression(lst[i+1:]) 
+            if lst[0] == '(':
+                lst[0] = self.arithmetic_expression(lst[1:]) 
                 # figuring out how far the parenthesis went
                 count = 1
                 numPar = 1
-                for x in range(len(lst[i+1:])):
-                    if x == '(': 
+                for i in range(1, len(lst[1:])):
+                    if lst[i] == '(': 
                         numPar += 1
-                    elif numPar == 1 and x == ')':
+                    elif numPar == 1 and lst[i] == ')':
                         break
-                    elif x == ')':
+                    elif lst[i] == ')':
                         numPar -= 1
                     count += 1
-                i += count
-                        
-            elif lst[i] == ')':
-                return str
-            else:
-                if lst[i].isdigit():
-                    str = "'" + lst[i+1] + "', " + lst[i] + ", "
+                lst = lst[:1] + lst[count+1:]
+            
+            elif lst[1] == ')':
+                return lst[0] 
+            
+            elif len(lst) > 3 and lst[2] == '(':
+                lst[2] = self.arithmetic_expression(lst[3:])
+                count = 1
+                numPar = 1
+                for i in range(3, len(lst[3:]) + 2):
+                    if lst[i] == '(': 
+                        numPar += 1
+                    elif numPar == 1 and lst[i] == ')':
+                        break
+                    elif lst[i] == ')':
+                        numPar -= 1
+                    count += 1
+                
+                #only case where lst[2] will be operation in parenthesis so separate print statements
+                if lst[0].isdigit():
+                    lst[0] = "'" + lst[1] + "', " + lst[0] + ", " + lst[2]
+                elif lst[0][0] == '(':
+                    lst[0] = "'" + lst[1] + "', " + lst[0] + ", " + lst[2]
                 else:
-                    str = "'" + lst[i+1] + "', '" + lst[i] + ", "
-                i += 2
+                    lst[0] = "'" + lst[1] + "', '" + lst[0] + "', " + lst[2]
 
-        return "(" + str + ")"
+                lst = lst[:1] + lst[count+3:]
+
+            else:
+                #This part is used for a normal statement without parenthesis within
+
+                #this is the first statement if it is a digit
+                if lst[0].isdigit(): 
+                    if lst[2].isdigit():
+                        lst[0] = "'" + lst[1] + "', " + lst[0] + ", " + lst[2]
+                    else:
+                        lst[0] = "'" + lst[1] + "', " + lst[0] + ", '" + lst[2] + "'"
+                    lst[0] = "(" + lst[0] + ")"
+                
+                #this is if there is more than one terms so lst[0] will be another formatted operation
+                elif lst[0][0] == '(':
+                    if lst[2].isdigit():
+                        lst[0] = "'" + lst[1] + "', " + lst[0] + ", " + lst[2]
+                    else:
+                        lst[0] = "'" + lst[1] + "', " + lst[0] + ", '" + lst[2] + "'"
+                    lst[0] = "(" + lst[0] + ")"
+                    #lst[0] = "'" + lst[1] + "', " + lst[0] + ", " + lst[2]
+                        
+                #this is the first statement if it is a variable and not a constant
+                else:
+                    if lst[2].isdigit():
+                        lst[0] = "'" + lst[1] + "', '" + lst[0] + "', " + lst[2] 
+                    else:
+                        lst[0] = "'" + lst[1] + "', '" + lst[0] + "', '" + lst[2] + "'"
+                    lst[0] = "(" + lst[0] + ")"
+                lst = lst[:1] + lst[3:]
+
+
+        return lst[0] 
+
+   
+   # def term(self):
+    
+
+   # def factor(self):
+
+
+    # parse if statement, you can handle then and else part here.
+    # you also have to check for condition.
+    def if_statement(self):
+        return "w"
+    
+    # implement while statment, check for condition
+    # possibly make a call to statement?
+    def while_loop(self):
+        return "w"
+
+   # def condition(self):
+  
